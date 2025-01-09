d9278d161a6a46d2f8785048cd0b7804225aba4d
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-01-09 11:28:21 +0100

    styled contact-page

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -32,10 +32,10 @@ import { render } from "storyblok-rich-text-react-renderer";
 
 const Contact = ({ blok }: any) => {
   return (
-    <div>
+    <div className="lg:mt-16 lg:mb-16">
       <h1 className="px-32 text-[22px] uppercase">{blok.heading}</h1>
 
-      <div className="w-[100vw] text-center lg:text-start px-6 lg:flex lg:px-32 mt-10 text-[18px]">
+      <div className="w-[100vw] lg:h-[100vh] text-center lg:text-start px-6 lg:flex lg:px-32 mt-10 text-[18px]">
         <div>
           <div className="lg:w-[80%] mb-10">{render(blok.info)}</div>
           <div>{render(blok.business_information)}</div>
