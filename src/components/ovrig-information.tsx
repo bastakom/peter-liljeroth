4d7e7e1b63e4d502f52cd7ae6327e5467161eabe
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-01-13 10:12:46 +0100

    small fix

--- src/components/ovrig-information.tsx
+++ src/components/ovrig-information.tsx
@@ -13,9 +13,9 @@ import { render } from "storyblok-rich-text-react-renderer";
 const OtherInformation = ({ blok }: OtherInformationProps) => {
   return (
     <div className="px-12 mt-4" {...storyblokEditable}>
-      <div className="flex flex-col gap-3 other-information">
+      <div className="flex flex-col gap-3">
         <h1 className="text-3xl font-semibold pb-4">{blok.title}</h1>
-        {render(blok.content)}
+        <div className="font-inter-normal">{render(blok.content)}</div>
       </div>
     </div>
   );
--- src/components/ui/footer/footerSection.tsx
+++ src/components/ui/footer/footerSection.tsx
@@ -123,7 +123,7 @@ export const FooterSection = ({ props }: FooterProps) => {
       <div className="font-dupincel text-[#FFFFFF] flex flex-col text-center lg:text-start mb-2 lg:mb-0 lg:flex-row p-6 gap-5 lg:justify-end lg:p-0 lg:px-10 text-[18px] lg:text-[22px] mt-4 lg:mt-40">
         <Link href={"#"}>Integritetspolicy</Link>
         <Link href={"#"}>Cookies</Link>
-        <Link href={"#"}>Övrig information</Link>
+        <Link href={"/oevrig-information"}>Övrig information</Link>
       </div>
 
       <div className="text-[#FFFFFF] justify-center pb-4 flex lg:justify-end lg:pb-0 lg:px-10 text-[18px] lg:text-[22px] mt-0 lg:mt-6">
