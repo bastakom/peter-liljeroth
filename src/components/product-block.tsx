9b5930b49611945ea4e319acb6f9778db208d554
Philip <anlanderp@gmail.com>
2025-02-07 15:13:34 +0100

    change bg if not on product

--- src/components/product-block.tsx
+++ src/components/product-block.tsx
@@ -25,10 +25,10 @@ export const ProductBlock = ({ blok }: any) => {
           return (
             <button
               key={item.uuid}
-              className="flex flex-col gap-4 bg-[white] lg:h-[50vh]"
+              className="flex flex-col gap-4 lg:h-[50vh]"
               onClick={() => handleOpenModal(item.uuid)}
             >
-              <div className="w-full h-[45vh] gap-4 lg:h-[100%] 3xl:h-[600px] relative hidden md:flex">
+              <div className="w-full h-[45vh] gap-4 lg:h-[100%] 3xl:h-[600px] relative hidden md:flex bg-[#fbf9f6]">
                 <Image
                   src={item?.content?.image?.filename || ""}
                   alt=""
